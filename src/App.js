a637ed25831bc6951ff9141f9e370e458283f205
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-16 17:45:24 +0100

    display all required elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 import { Length } from "./components/Length";
 
 export const App = () => {
+  const [time, setTime] = useState(25);
+
   return (
     <div>
       <h1 className="center">25 + 5 Clock</h1>
-      <Length labelId="break-label" title="Break" lengthDuration="5" incrementId="break-increment" decrementId="break-decrement" />
-      <Length labelId="session-label" title="Session" lengthDuration="25" incrementId="session-increment" decrementId="session-decrement" />
+      <Length
+        labelId="break-label"
+        title="Break"
+        lengthDuration="5"
+        incrementId="break-increment"
+        decrementId="break-decrement"
+      />
+      <Length
+        labelId="session-label"
+        title="Session"
+        lengthDuration="25"
+        incrementId="session-increment"
+        decrementId="session-decrement"
+      />
+      <div>
+        <p id="timer-label">Session</p>
+        <p id="timer-left">{time}</p>
+      </div>
+
+      <div>
+        <button id="start-stop">Start</button>
+        <button id="reset">Reset</button>
+      </div>
     </div>
   );
 };
