d881621a55850600b057b669afbab4e89877c15f
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-18 12:17:02 +0100

    pass all tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import audioBeep from "./assets/beep.wav";
 import { Length } from "./components/Length";
 
 export const App = () => {
   const [isRunning, setIsRunning] = useState(false);
+
+  const [currentRunning, setCurrentRunning] = useState("Session");
+
+  const [sessionLength, setSessionLength] = useState(25);
+  const [breakLength, setBreakLength] = useState(5);
 
   const [state, setState] = useState({
     minutes: 25,
@@ -14,13 +20,61 @@ export const App = () => {
     setIsRunning(!isRunning);
   };
 
+  const resetTimer = () => {
+    console.log("reste");
+    setCurrentRunning("Session");
+    setIsRunning(false);
+    setSessionLength(25);
+    setBreakLength(5);
+    setState({ minutes: 25, seconds: 0 });
+    const audioElement = document.getElementById("beep");
+    audioElement.pause();
+    audioElement.currentTime = 0;
+  };
+
+  const decrementSession = () => {
+    if (sessionLength - 1 <= 0) {
+      return;
+    }
+    setSessionLength(sessionLength - 1);
+    setState({ ...state, minutes: sessionLength - 1 });
+  };
+
+  const incrementSession = () => {
+    if (sessionLength + 1 > 60) {
+      return;
+    }
+    setSessionLength(sessionLength + 1);
+    setState({ ...state, minutes: sessionLength + 1 });
+  };
+
+  const decrementBreak = () => {
+    if (breakLength - 1 <= 0) {
+      return;
+    }
+
+    setBreakLength(breakLength - 1);
+    // setState({...state, minutes: sessionLength - 1});
+  };
+
+  const incrementBreak = () => {
+    if (breakLength + 1 > 60) {
+      return;
+    }
+    setBreakLength(breakLength + 1);
+    // setState({ ...state, minutes: sessionLength + 1 });
+  };
+
+  const playAudioBeep = () => {
+    const audioElement = document.getElementById("beep");
+    audioElement.play();
+  };
+
   useEffect(() => {
     let interval = null;
     if (isRunning) {
-       interval = setInterval(() => {
+      interval = setInterval(() => {
         const { seconds, minutes } = state;
-
-        console.log(state);
 
         if (seconds > 0) {
           const newState = { minutes: minutes, seconds: seconds - 1 };
@@ -30,6 +84,15 @@ export const App = () => {
         if (seconds === 0) {
           if (minutes === 0) {
             clearInterval(interval);
+            if (currentRunning === "Session") {
+              setCurrentRunning("Break");
+              setState({ minutes: breakLength, seconds: 0 });
+              playAudioBeep();
+            } else {
+              setCurrentRunning("Session");
+              setState({ minutes: sessionLength, seconds: 0 });
+              playAudioBeep();
+            }
           } else {
             const newState = { minutes: minutes - 1, seconds: 59 };
             setState(newState);
@@ -38,7 +101,7 @@ export const App = () => {
       }, 1000);
     }
     return () => clearInterval(interval);
-  }, [state, isRunning]);
+  }, [state, isRunning, breakLength, currentRunning, sessionLength]);
 
   return (
     <div>
@@ -46,32 +109,41 @@ export const App = () => {
       <div className="control-container">
         <Length
           labelId="break-label"
+          spanId="break-length"
           title="Break"
-          lengthDuration="5"
+          lengthDuration={breakLength}
           incrementId="break-increment"
           decrementId="break-decrement"
+          decrement={decrementBreak}
+          increment={incrementBreak}
         />
         <Length
           labelId="session-label"
+          spanId="session-length"
           title="Session"
-          lengthDuration="25"
+          lengthDuration={sessionLength}
           incrementId="session-increment"
           decrementId="session-decrement"
+          decrement={decrementSession}
+          increment={incrementSession}
         />
       </div>
       <div className="session-container">
-        <p id="timer-label">Session</p>
-        <p id="timer-left">
-          {state.minutes}:
+        <p id="timer-label">{currentRunning}</p>
+        <p id="time-left">
+          {state.minutes < 10 ? `0${state.minutes}` : state.minutes}:
           {state.seconds < 10 ? `0${state.seconds}` : state.seconds}
         </p>
       </div>
       <div className="control-container">
-        <button id="start-stop" onClick={startTimer}>
+        <button id="start_stop" onClick={startTimer}>
           Start
         </button>
-        <button id="reset">Reset</button>
+        <button id="reset" onClick={resetTimer}>
+          Reset
+        </button>
       </div>
+      <audio style={{ display: "none" }} id="beep" src={audioBeep}></audio>
     </div>
   );
 };
--- src/components/Length.js
+++ src/components/Length.js
@@ -4,18 +4,22 @@ export const Length = ({
   title,
   lengthDuration,
   labelId,
+  spanId,
   incrementId,
   decrementId,
+  decrement,
+  increment
 }) => {
   const [duration, setDuration] = useState(lengthDuration);
+
 
   return (
     <div>
       <h3 id={labelId}>{title} Length</h3>
       <div className="controls-length">
-        <span id={incrementId}>+</span>
-        <span>{lengthDuration}</span>
-        <span id={decrementId}>-</span>
+        <span onClick={increment} id={incrementId}>+</span>
+        <span id={spanId}>{lengthDuration}</span>
+        <span onClick={decrement} id={decrementId}>-</span>
       </div>
     </div>
   );
